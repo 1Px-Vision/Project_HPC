9d19381836a7f321d1dcca34e19918e35c1f375c
1Px-Vision <150855410+1Px-Vision@users.noreply.github.com>
2025-03-08 20:00:55 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy
+
+ext_modules = [
+    Extension(
+        name="cython_cuda_fft",
+        sources=["cython_cuda_fft.pyx"],
+        libraries=["cudart"],
+        library_dirs=["/usr/local/cuda/lib64"],
+        include_dirs=[numpy.get_include(), "/usr/local/cuda/include"],
+        language="c++",
+        extra_compile_args=['-O3'],
+        extra_link_args=['-lcudart', '-L/usr/local/cuda/lib64'],
+    ),
+]
+
+setup(
+    name="cython_cuda_fft",
+    ext_modules=cythonize(ext_modules, compiler_directives={'language_level': "3"}),
+)
